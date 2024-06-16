8a7b3d75e8eeb6c73fad9d727f82bcebd4ca4ac8
orannahum <oranne5@gmail.com>
2024-06-16 17:54:52 +0300

    fix for badim 1.11111

--- predict_sales.py
+++ predict_sales.py
@@ -17,6 +17,9 @@ start_date = '2023-05-01'
 end_date = '2024-06-30'
 agg_time_freq = 'M'
 items = ['20009902', '20004028']
+digits_0_2 = []
+digits_2_5 = []
+digits_2_8 = []
 inv_mov_types = ['החזרה מלקוח','חשבוניות מס', 'דאטה מסאפ','משלוחים ללקוח']
 def filter_for_query(name_of_column, filter_list):
     if len(filter_list) == 0:
@@ -25,6 +28,13 @@ def filter_for_query(name_of_column, filter_list):
         # add quotes to string values
         formatted_values = ", ".join([f"'{x}'" if isinstance(x, str) else str(x) for x in filter_list])
         return f"AND {name_of_column} IN ({formatted_values})"
+def filter_from_right_item_charachters(filter_list, index_of_first_char, lenght_of_chars, name_of_column='item'):
+    if len(filter_list) == 0:
+        return ''
+    else:
+        formatted_values = ", ".join([f"'{x}'" if isinstance(x, str) else str(x) for x in filter_list])
+        return f"AND SUBSTRING({name_of_column}, {index_of_first_char + 1}, {lenght_of_chars}) IN ({formatted_values})"
+
 
 print(password, username, port, host)
 client_name = 'badim'
@@ -61,21 +71,37 @@ if __name__ == "__main__":
                                       where toDate(cur_date) >= '{start_date}' and toDate(cur_date) <= '{end_date}'
                                         {filter_for_query('inv_mov_type', inv_mov_types)}
                                         {filter_for_query('item', items)}
+                                                                    {filter_from_right_item_charachters(digits_0_2, 0, 2)}
+                             {filter_from_right_item_charachters(digits_2_5, 2, 3)}
+                             {filter_from_right_item_charachters(digits_2_8, 2, 6)}
                                       '''
     print(query)
     sales_df = client.query_dataframe(query)
+    # i want add unique id ass hirrercy of the first 2 digits
+
 
     print(sales_df.head())
     print(sales_df.columns)
 
-
+    df_final = pd.DataFrame(columns=['ds', 'unique_id', 'y'])
     # Filter and prepare data
     sales_df['cur_date'] = pd.to_datetime(sales_df['cur_date'])
     sales_df['quantity'] = sales_df['quantity'].astype(float)
     sales_df_grouped = sales_df.groupby(['cur_date', 'item'])['quantity'].sum().unstack().fillna(0).stack().reset_index()
     # group by agg_time_freq
     sales_df_grouped = sales_df_grouped.groupby(['item', pd.Grouper(key='cur_date', freq=agg_time_freq)])[0].sum().reset_index()
-    sales_df_grouped = sales_df_grouped.rename(columns={'cur_date': 'ds', 'item': 'unique_id', 0: 'y'})
+    sales_df_grouped['ch_0_2'] = sales_df_grouped['item'].str[0:2] + '_category'
+    sales_df_grouped['ch_2_5'] = sales_df_grouped['item'].str[2:5] + '_catalog'
+    sales_df_grouped['ch_2_8'] = sales_df_grouped['item'].str[2:8] + '_catalog_color'
+
+    #sales_df_grouped = sales_df_grouped.rename(columns={'cur_date': 'ds', 'item': 'unique_id', 0: 'y'})
+    sales_df_grouped1 = sales_df_grouped.rename(columns={'cur_date': 'ds', 'item': 'unique_id', 0: 'y'})[['ds', 'unique_id', 'y']]
+    sales_df_grouped2 = sales_df_grouped.rename(columns={'cur_date': 'ds', 'ch_0_2': 'unique_id', 0: 'y'})[['ds', 'unique_id', 'y']]
+    sales_df_grouped3 = sales_df_grouped.rename(columns={'cur_date': 'ds', 'ch_2_5': 'unique_id', 0: 'y'})[['ds', 'unique_id', 'y']]
+    sales_df_grouped4 = sales_df_grouped.rename(columns={'cur_date': 'ds', 'ch_2_8': 'unique_id', 0: 'y'})[['ds', 'unique_id', 'y']]
+    sales_df_grouped = pd.concat([sales_df_grouped1, sales_df_grouped2, sales_df_grouped3, sales_df_grouped4], axis=0).groupby(['ds', 'unique_id']).sum().reset_index()
+
+
 
     print("sales_df_grouped", sales_df_grouped)
 
