a09a0e9095e50cb086d91438941d44bcc2851808
orannahum <oranne5@gmail.com>
2024-06-18 10:46:54 +0300

    fix for badim 1.11111111

--- sdtla/check_results.py
+++ sdtla/check_results.py
@@ -5,7 +5,4 @@ import json
 with open('best_no_gradient_models.json', 'r') as f:
     best_no_gradient_models = json.load(f)
 
-print(best_no_gradient_models)
-for key, value in best_no_gradient_models.items():
-    print(key)
-    print(pd.DataFrame(value))
+print(len(best_no_gradient_models))
